1c930963b9c7451a88bdef4fe5852824f033f96c
Rajneesh <30917680+RajneeshKV@users.noreply.github.com>
2019-01-10 23:04:49 +0530

    Add files via upload

--- u_demo_1/src/CommentDetails.js
+++ u_demo_1/src/CommentDetails.js
@@ -1,18 +1,19 @@
 import React from "react";
-import faker from "faker";
+//import faker from "faker";
 
 const CommentDetails = props => {
-  console.log(props);
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.avatar()} />
+        <img alt="avatar" src={props.pic} />
       </a>
       <div className="content">
         <a href="/" className="author">
           {props.author}
         </a>
         <div className="metadata">
+          <span className="date">{props.timeAgo}</span>
+
           <span className="date">10:56 PLM</span>
         </div>
         <div className="text">{props.mycomment}</div>
